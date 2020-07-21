f7a0cb94398ebca974bfd54b83bd3660fa8ae403
Abdulrahman Basalamah <42208656+BasalamahA@users.noreply.github.com>
2020-07-21 14:51:03 +0300

    Update update.bash

--- update.bash
+++ update.bash
@@ -1,17 +1,20 @@
-figlet -f slant "Update Software" | lolcat
+echo "    ______      ____                   __      __       "
+echo "   / ____/_  __/ / /  __  ______  ____/ /___ _/ /____   "
+echo "  / /_  / / / / / /  / / / / __ \/ __  / __ \`/ __/ _ \       By: Basalamah@hotmail.com"
+echo " / __/ / /_/ / / /  / /_/ / /_/ / /_/ / /_/ / /_/  __/  "
+echo "/_/    \__,_/_/_/   \__,_/ .___/\__,_/\__,_/\__/\___/   "
+echo "                        /_/                             "
 
-echo "Welcome to the update software !!" | lolcat
-echo "Starting the update !!" | lolcat 
+
+echo "Welcome to the update software !!"
+echo "Starting the update !!"
 
 sudo apt-get update
 
-echo "Update is done !!" | lolcat  
-echo "Starting upgrade !!" | lolcat
+echo "Update is done !!"
+echo "Starting upgrade !!"
 
 sudo apt-get upgrade -y
 
-echo "Upgrade is done !!" | lolcat
-echo "We are all set ....." | lolcat 
-
-
-
+echo "Upgrade is done !!"
+echo "We are all set ....."
